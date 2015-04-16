48d52728b1d0925db334efa91512bbadf02f0c68
Pete Nellius <pete.nellius@ogilvy.com>
2015-04-16 19:27:30 -0400

    added swift file

---
+++ MidtermCalculator/MidtermCalculator/OperatorFunctions.swift
@@ -0,0 +1,10 @@
+//
+//  OperatorFunctions.swift
+//  MidtermCalculator
+//
+//  Created by Pete Nellius on 4/16/15.
+//  Copyright (c) 2015 Pete Nellius. All rights reserved.
+//
+
+import Foundation
+
